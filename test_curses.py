bd0087462c165917418bab02c08cd5caa0b52467
gyopiter <22850165+gyopiter@users.noreply.github.com>
2022-03-14 00:13:22 +0900

    test

--- test_curses.py
+++ test_curses.py
@@ -1,14 +1,23 @@
-import curses
 from curses import wrapper
-from textwrap import wrap
+import wscheck
 
 def main(stdscr):
+    ws = wscheck.wscheck()
+
     stdscr.clear()
-    for i in range(50): stdscr.addstr(i, 0, str(i))
-    for i in range(44):
-        stdscr.addstr(8+4*int(i/6), 8+20*int(i%6), "std1dc"+str(i+2))
+    for i in range(1, 53):
+        if i == 1: stdscr.addstr(2, 38, 'std3dc1')
+        elif i == 52: stdscr.addstr(2, 78, 'std3dc52') 
+        else: stdscr.addstr(8+4*int((i-2)/6), 8+20*int((i-2)%6), "std3dc"+str(i))
+
     stdscr.refresh()
-    stdscr.getkey()
+    for i in range(1, 53):
+        if i == 1: stdscr.addstr(3, 38, ws.get_userid('std3', 1))
+        elif i == 52: stdscr.addstr(3, 78, ws.get_userid('std3', 52))
+        else: stdscr.addstr(9+4*int((i-2)/6), 8+20*int((i-2)%6), ws.get_userid('std3', i+2))
+        stdscr.refresh()
+
+    input()
 
 if __name__ == "__main__":
     wrapper(main)
